f8ca18262b5eea355c6a473465a9cfa5fa425b53
Gabs.exe Failed to Load <112909127+GabrielRugila@users.noreply.github.com>
2022-09-17 17:09:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+'use strict';
+
+// elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const currentS0 = document.getElementById('current--0');
+const currentS1 = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// create single-player mode
+
+//declare variables
+let scores, playing, currentScore, activePlayer;
+
+// initialization
+const init = function () {
+  scores = [0, 0];
+  playing = true;
+  currentScore = 0;
+  activePlayer = 0;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  currentS0.textContent = 0;
+  currentS1.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
+
+// active
+
+function changeActive() {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+}
+
+// score
+
+//dice
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    let diceRoll = Math.trunc(Math.random() * 6) + 1;
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${diceRoll}.png`;
+    if (diceRoll != 1) {
+      currentScore += diceRoll;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      changeActive();
+    }
+  }
+});
+
+//hold
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      changeActive();
+    }
+  }
+});
+
+//reset
+btnNew.addEventListener('click', init);
